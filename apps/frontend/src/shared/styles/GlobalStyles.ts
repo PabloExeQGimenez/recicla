4d3222227e4dc44084a08f87b3aa4e6c0c22c9d3
Pablo Gimenez <pablogimenez.er@gmail.com>
2026-07-15 03:16:00 -0300

    fix: arreglo en GlobalStyles

--- apps/frontend/src/shared/styles/GlobalStyles.ts
+++ apps/frontend/src/shared/styles/GlobalStyles.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
-const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 
+const GlobalStyles = createGlobalStyle`
 * {
   margin: 0;
   padding: 0;
@@ -11,13 +9,11 @@ const GlobalStyles = createGlobalStyle`
 
 body {
   font-family: "Montserrat", serif;
-  background-color: ${({ theme }) => theme.colors.background};
-  /* background: 
-    linear-gradient(135deg, rgba(19, 19, 19, 0.6), rgba(129, 129, 130, 0.8)); */
+  background-color: ${({ theme }) => theme.colors.surface.app};
   background-size: cover;
   background-repeat: repeat;
   background-blend-mode: overlay;
-  color: ${({ theme }) => theme.colors.text};
+  color: ${({ theme }) => theme.colors.text.primary};
   height: 100vh;
   overflow: hidden;
   position: relative;
